36cbcec34562d33b187e8625fb3c6280ddf95eb6
Auracule <daabzpinky@gmail.com>
2023-04-25 15:00:12 +0100

    update for deploy

--- shopping/settings.py
+++ shopping/settings.py
@@ -140,5 +140,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "pogooluwa12@gmail.com"
-EMAIL_HOST_PASSWORD = "tkoqqedagwxyzgir"
+EMAIL_HOST_USER = "daabzpinky@@gmail.com"
+EMAIL_HOST_PASSWORD = "tfcrsghzjpgqdkqo"
