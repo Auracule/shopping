2169a46d79b2144f990fc8981e450fe5135f3183
Auracule <daabzpinky@gmail.com>
2023-04-25 13:17:41 +0100

    update for deploy

--- shopping/settings.py
+++ shopping/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-1erve9at9e5if*7-jy77h5045@^t+v_vd*x(=cz-@3vl&yenp!
 DEBUG = False
 # DEBUG = True
 
-ALLOWED_HOSTS = ['16.171.59.89', '*']
+ALLOWED_HOSTS = ['13.48.196.30', '*']
 
 
 # Application definition
